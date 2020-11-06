192dd103b646457d332d99b9fb6ea1965c4a3b34
Eneki <joanreneki@gmail.com>
2020-11-06 18:24:25 +0300

    Final fix

--- assets/js/books.js
+++ assets/js/books.js
@@ -39,14 +39,10 @@ $(function () {
         // In the absence of a second argument, the helper object becomes 'b' while 'options' remains undefined
         if (typeof b === 'object') {
             with(this) {
-                let arrayWithObjectsWhoseKeyMatchesTheExpressionValue = main.books.filter(
-                    book => Object.values(book).some(function (key) {
-                        return key === a;
-                    }));
-                let oneObject = arrayWithObjectsWhoseKeyMatchesTheExpressionValue[0]
-
-                const getKey = (obj, val) => Object.keys(obj).find(key => obj[key] === val);
-                let correspondingKey = getKey(oneObject, a);
+                function getCorrespondingKey(obj, val) {
+                    return Object.keys(obj).find(key => obj[key] === val);
+                }
+                let correspondingKey = getCorrespondingKey(this, a);
                 result = correspondingKey
             }
 
